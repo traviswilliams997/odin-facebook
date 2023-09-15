1df217fe3b5d6d5ae38cd3a34c51d34c25b76343
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-15 02:44:18 -0500

    Add user profile page
    - [x] Create users controller with show action
    - [x] Create route to show
    - [x] Test that route works
    - [x] Create show page that displays user information

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  resources :users, only: [:show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
