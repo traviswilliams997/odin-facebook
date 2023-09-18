0b9e19056b595eebd189be706f50141eb3852598
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-17 23:37:29 -0500

    Add profile photo
    - [x] Install mini magic
    - [x] Update active storage to use mini magic
    - [x] Update registrations controller to permit user profile avatar
    - [x] Update edit form to accept user profile avatar
    - [x] Update show form to display user profile avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes
-
+  has_one_attached :avatar
 
 
   followability
